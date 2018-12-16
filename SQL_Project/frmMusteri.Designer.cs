f0740368e27f0a22ff7dc86cd0bbeda44996ad57
emrecavdar <emreavdar@gmail.com>
2018-12-16 20:53:34 +0300

    araclar ve musteri ekleme güncelleme islemleri

--- SQL_Project/frmMusteri.Designer.cs
+++ SQL_Project/frmMusteri.Designer.cs
@@ -36,17 +36,16 @@
             this.lblMusteriTCNo = new System.Windows.Forms.Label();
             this.tbMusteriAdres = new System.Windows.Forms.TextBox();
             this.tbMusteriEPosta = new System.Windows.Forms.TextBox();
-            this.tbMusteriTelNo = new System.Windows.Forms.TextBox();
-            this.tbMusteriTCNo = new System.Windows.Forms.TextBox();
             this.gbGenelBilgiler = new System.Windows.Forms.GroupBox();
             this.lblMusteriSoyad = new System.Windows.Forms.Label();
             this.lblMusteriAd = new System.Windows.Forms.Label();
-            this.lblMusteriNo = new System.Windows.Forms.Label();
             this.tbMusteriSoyad = new System.Windows.Forms.TextBox();
             this.tbMusteriAd = new System.Windows.Forms.TextBox();
-            this.tbMusteriNo = new System.Windows.Forms.TextBox();
             this.btnEkle = new System.Windows.Forms.Button();
             this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.tbTcNo = new System.Windows.Forms.MaskedTextBox();
+            this.tbMusTelNo = new System.Windows.Forms.MaskedTextBox();
             this.gbKisiselBilgiler.SuspendLayout();
             this.gbGenelBilgiler.SuspendLayout();
             this.SuspendLayout();
@@ -54,18 +53,19 @@
             // gbKisiselBilgiler
             // 
             this.gbKisiselBilgiler.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.gbKisiselBilgiler.Controls.Add(this.tbMusTelNo);
+            this.gbKisiselBilgiler.Controls.Add(this.tbTcNo);
+            this.gbKisiselBilgiler.Controls.Add(this.btnGetir);
             this.gbKisiselBilgiler.Controls.Add(this.lblMusteriAdres);
             this.gbKisiselBilgiler.Controls.Add(this.lblMusteriEPosta);
             this.gbKisiselBilgiler.Controls.Add(this.lblMusteriTelNo);
             this.gbKisiselBilgiler.Controls.Add(this.lblMusteriTCNo);
             this.gbKisiselBilgiler.Controls.Add(this.tbMusteriAdres);
             this.gbKisiselBilgiler.Controls.Add(this.tbMusteriEPosta);
-            this.gbKisiselBilgiler.Controls.Add(this.tbMusteriTelNo);
-            this.gbKisiselBilgiler.Controls.Add(this.tbMusteriTCNo);
             this.gbKisiselBilgiler.Location = new System.Drawing.Point(371, 15);
-            this.gbKisiselBilgiler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbKisiselBilgiler.Margin = new System.Windows.Forms.Padding(4);
             this.gbKisiselBilgiler.Name = "gbKisiselBilgiler";
-            this.gbKisiselBilgiler.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbKisiselBilgiler.Padding = new System.Windows.Forms.Padding(4);
             this.gbKisiselBilgiler.Size = new System.Drawing.Size(347, 215);
             this.gbKisiselBilgiler.TabIndex = 0;
             this.gbKisiselBilgiler.TabStop = false;
@@ -114,7 +114,7 @@
             // tbMusteriAdres
             // 
             this.tbMusteriAdres.Location = new System.Drawing.Point(141, 119);
-            this.tbMusteriAdres.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbMusteriAdres.Margin = new System.Windows.Forms.Padding(4);
             this.tbMusteriAdres.Multiline = true;
             this.tbMusteriAdres.Name = "tbMusteriAdres";
             this.tbMusteriAdres.Size = new System.Drawing.Size(196, 88);
@@ -123,40 +123,22 @@
             // tbMusteriEPosta
             // 
             this.tbMusteriEPosta.Location = new System.Drawing.Point(141, 87);
-            this.tbMusteriEPosta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbMusteriEPosta.Margin = new System.Windows.Forms.Padding(4);
             this.tbMusteriEPosta.Name = "tbMusteriEPosta";
             this.tbMusteriEPosta.Size = new System.Drawing.Size(196, 22);
             this.tbMusteriEPosta.TabIndex = 5;
-            // 
-            // tbMusteriTelNo
-            // 
-            this.tbMusteriTelNo.Location = new System.Drawing.Point(141, 55);
-            this.tbMusteriTelNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tbMusteriTelNo.Name = "tbMusteriTelNo";
-            this.tbMusteriTelNo.Size = new System.Drawing.Size(196, 22);
-            this.tbMusteriTelNo.TabIndex = 4;
-            // 
-            // tbMusteriTCNo
-            // 
-            this.tbMusteriTCNo.Location = new System.Drawing.Point(141, 23);
-            this.tbMusteriTCNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tbMusteriTCNo.Name = "tbMusteriTCNo";
-            this.tbMusteriTCNo.Size = new System.Drawing.Size(196, 22);
-            this.tbMusteriTCNo.TabIndex = 3;
             // 
             // gbGenelBilgiler
             // 
             this.gbGenelBilgiler.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.gbGenelBilgiler.Controls.Add(this.lblMusteriSoyad);
             this.gbGenelBilgiler.Controls.Add(this.lblMusteriAd);
-            this.gbGenelBilgiler.Controls.Add(this.lblMusteriNo);
             this.gbGenelBilgiler.Controls.Add(this.tbMusteriSoyad);
             this.gbGenelBilgiler.Controls.Add(this.tbMusteriAd);
-            this.gbGenelBilgiler.Controls.Add(this.tbMusteriNo);
             this.gbGenelBilgiler.Location = new System.Drawing.Point(16, 15);
-            this.gbGenelBilgiler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbGenelBilgiler.Margin = new System.Windows.Forms.Padding(4);
             this.gbGenelBilgiler.Name = "gbGenelBilgiler";
-            this.gbGenelBilgiler.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbGenelBilgiler.Padding = new System.Windows.Forms.Padding(4);
             this.gbGenelBilgiler.Size = new System.Drawing.Size(347, 122);
             this.gbGenelBilgiler.TabIndex = 1;
             this.gbGenelBilgiler.TabStop = false;
@@ -165,7 +147,7 @@
             // lblMusteriSoyad
             // 
             this.lblMusteriSoyad.AutoSize = true;
-            this.lblMusteriSoyad.Location = new System.Drawing.Point(8, 91);
+            this.lblMusteriSoyad.Location = new System.Drawing.Point(8, 58);
             this.lblMusteriSoyad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMusteriSoyad.Name = "lblMusteriSoyad";
             this.lblMusteriSoyad.Size = new System.Drawing.Size(98, 17);
@@ -175,46 +157,28 @@
             // lblMusteriAd
             // 
             this.lblMusteriAd.AutoSize = true;
-            this.lblMusteriAd.Location = new System.Drawing.Point(8, 59);
+            this.lblMusteriAd.Location = new System.Drawing.Point(8, 26);
             this.lblMusteriAd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMusteriAd.Name = "lblMusteriAd";
             this.lblMusteriAd.Size = new System.Drawing.Size(75, 17);
             this.lblMusteriAd.TabIndex = 4;
             this.lblMusteriAd.Text = "Müşteri Ad";
             // 
-            // lblMusteriNo
-            // 
-            this.lblMusteriNo.AutoSize = true;
-            this.lblMusteriNo.Location = new System.Drawing.Point(8, 27);
-            this.lblMusteriNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblMusteriNo.Name = "lblMusteriNo";
-            this.lblMusteriNo.Size = new System.Drawing.Size(118, 17);
-            this.lblMusteriNo.TabIndex = 3;
-            this.lblMusteriNo.Text = "Müşteri Numarası";
-            // 
             // tbMusteriSoyad
             // 
-            this.tbMusteriSoyad.Location = new System.Drawing.Point(141, 87);
-            this.tbMusteriSoyad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbMusteriSoyad.Location = new System.Drawing.Point(141, 54);
+            this.tbMusteriSoyad.Margin = new System.Windows.Forms.Padding(4);
             this.tbMusteriSoyad.Name = "tbMusteriSoyad";
             this.tbMusteriSoyad.Size = new System.Drawing.Size(196, 22);
             this.tbMusteriSoyad.TabIndex = 2;
             // 
             // tbMusteriAd
             // 
-            this.tbMusteriAd.Location = new System.Drawing.Point(141, 55);
-            this.tbMusteriAd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbMusteriAd.Location = new System.Drawing.Point(141, 22);
+            this.tbMusteriAd.Margin = new System.Windows.Forms.Padding(4);
             this.tbMusteriAd.Name = "tbMusteriAd";
             this.tbMusteriAd.Size = new System.Drawing.Size(196, 22);
             this.tbMusteriAd.TabIndex = 1;
-            // 
-            // tbMusteriNo
-            // 
-            this.tbMusteriNo.Location = new System.Drawing.Point(141, 23);
-            this.tbMusteriNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tbMusteriNo.Name = "tbMusteriNo";
-            this.tbMusteriNo.Size = new System.Drawing.Size(196, 22);
-            this.tbMusteriNo.TabIndex = 0;
             // 
             // btnEkle
             // 
@@ -224,6 +188,7 @@
             this.btnEkle.TabIndex = 2;
             this.btnEkle.Text = "Ekle";
             this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
             // 
             // btnGuncelle
             // 
@@ -233,6 +198,33 @@
             this.btnGuncelle.TabIndex = 3;
             this.btnGuncelle.Text = "Güncelle";
             this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(299, 21);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(38, 24);
+            this.btnGetir.TabIndex = 11;
+            this.btnGetir.Text = "...";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // tbTcNo
+            // 
+            this.tbTcNo.Location = new System.Drawing.Point(141, 21);
+            this.tbTcNo.Mask = "00000000000";
+            this.tbTcNo.Name = "tbTcNo";
+            this.tbTcNo.Size = new System.Drawing.Size(164, 22);
+            this.tbTcNo.TabIndex = 12;
+            // 
+            // tbMusTelNo
+            // 
+            this.tbMusTelNo.Location = new System.Drawing.Point(141, 58);
+            this.tbMusTelNo.Mask = "+\\9\\0999-999-99-99";
+            this.tbMusTelNo.Name = "tbMusTelNo";
+            this.tbMusTelNo.Size = new System.Drawing.Size(196, 22);
+            this.tbMusTelNo.TabIndex = 13;
             // 
             // frmMusteri
             // 
@@ -244,7 +236,7 @@
             this.Controls.Add(this.gbGenelBilgiler);
             this.Controls.Add(this.gbKisiselBilgiler);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMusteri";
@@ -262,21 +254,20 @@
 
         private System.Windows.Forms.GroupBox gbKisiselBilgiler;
         private System.Windows.Forms.GroupBox gbGenelBilgiler;
-        private System.Windows.Forms.TextBox tbMusteriNo;
-        private System.Windows.Forms.TextBox tbMusteriTCNo;
         private System.Windows.Forms.TextBox tbMusteriSoyad;
         private System.Windows.Forms.TextBox tbMusteriAd;
         private System.Windows.Forms.TextBox tbMusteriAdres;
         private System.Windows.Forms.TextBox tbMusteriEPosta;
-        private System.Windows.Forms.TextBox tbMusteriTelNo;
         private System.Windows.Forms.Label lblMusteriAdres;
         private System.Windows.Forms.Label lblMusteriEPosta;
         private System.Windows.Forms.Label lblMusteriTelNo;
         private System.Windows.Forms.Label lblMusteriTCNo;
         private System.Windows.Forms.Label lblMusteriSoyad;
         private System.Windows.Forms.Label lblMusteriAd;
-        private System.Windows.Forms.Label lblMusteriNo;
         private System.Windows.Forms.Button btnEkle;
         private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.MaskedTextBox tbTcNo;
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.MaskedTextBox tbMusTelNo;
     }
 }
--- SQL_Project/frmMusteri.cs
+++ SQL_Project/frmMusteri.cs
@@ -20,5 +20,48 @@ namespace SQL_Project
             this.baglanti = baglanti;
             InitializeComponent();
         }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string query = "insert into musteri values('" + tbMusteriAd.Text + "','" + tbMusteriSoyad.Text + "','" + tbTcNo.Text + "','" + tbMusTelNo.Text + "','" + tbMusteriEPosta.Text + "','" + tbMusteriAdres.Text + "')";
+            SqlCommand cmd = new SqlCommand(query, baglanti);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Müşteri Eklendi Eklendi");
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE musteri SET ad = '"+tbMusteriAd.Text+"', soyad = '"+tbMusteriSoyad.Text+ "', telefon = '" + tbMusTelNo.Text + "',eposta = '" + tbMusteriEPosta.Text + "', adres = '" + tbMusteriAdres.Text + "' WHERE tckNo ='" + tbTcNo.Text + "'";
+            SqlCommand cmd = new SqlCommand(query, baglanti);
+            
+          
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Müşteri Güncellendi");
+
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            string query = "select ad,soyad,tckNo,telefon,eposta,adres from musteri where  tckNo = '"+tbTcNo.Text+"'";
+            using (SqlCommand cmd = new SqlCommand(query, baglanti))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read()) { 
+                    tbMusteriAd.Text = (string)reader[0];
+                    tbMusteriSoyad.Text = (string)reader[1];
+                    tbTcNo.Text = (string)reader[2];
+                    tbMusTelNo.Text= (string)reader[3];
+                    tbMusteriEPosta.Text = (string)reader[4];
+                    tbMusteriAdres.Text = (string)reader[5];
+                    }
+
+
+                    
+
+                }
+            }
+           
+        }
     }
 }
