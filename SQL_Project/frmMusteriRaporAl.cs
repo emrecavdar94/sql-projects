c4c51a128b3a9a00350cb70dfd2f75866cc20339
denizkara <denizkara_dnz@hotmail.com>
2018-12-20 15:53:31 +0300

    eksik dosyalar eklendi

---
+++ SQL_Project/frmMusteriRaporAl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_Project
+{
+    public partial class frmMusteriRaporAl : Form
+    {
+        private SqlConnection baglanti;
+        DataTable dtVeri;
+        public frmMusteriRaporAl(SqlConnection baglanti, DataTable dtVeri)
+        {
+            this.baglanti = baglanti;
+            this.dtVeri = dtVeri;
+            InitializeComponent();
+        }
+    }
+}
